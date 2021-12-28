c1b3bc5b7fa8aca64c0521ede8e37e03dc97b4c1
SmolinPavel <isingwithaz@gmail.com>
2021-12-28 20:43:03 +0300

    Rewrite Home page component to use function instead of a class

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import logo from 'logo.svg';
 import 'App.css';
@@ -8,46 +8,40 @@ import { Table } from 'components/Table';
 import { Title } from 'components/Title';
 import { TitleContext } from 'contexts/Title';
 
-export class Home extends Component {
-  state = {
-    appTitle: 'React 37!',
-    open: false,
+export const Home = () => {
+  const [appTitle, setAppTitle] = useState('React 37!');
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(prevOpen => !prevOpen);
   };
 
-  handleClick = () => {
-    this.setState({ open: !this.state.open });
+  const handleChange = e => {
+    setAppTitle(e.target.value);
   };
 
-  handleChange = e => {
-    this.setState({ appTitle: e.target.value });
-  };
-
-  render() {
-    return (
-      <TitleContext.Provider value={{ title: this.state.appTitle }}>
-        <div className="App">
-          <header className="App-header">
-            <Title>{this.state.appTitle}</Title>
-            <div>
-              <input value={this.state.appTitle} onChange={this.handleChange} />
-            </div>
-            <img data-type="logo" src={logo} className="App-logo" alt="logo" />
-            {this.state.open && (
-              <Counter
-                title={this.state.appTitle}
-                arrProp={['Good', 'Neutral', 'Bad']}
-                onTitleUpdate={() => this.setState({ appTitle: 'New Value' })}
-              />
-            )}
-            <div>
-              <button onClick={this.handleClick}>
-                {this.state.open ? 'Close' : 'Open'}
-              </button>
-            </div>
-            <Table />
-          </header>
-        </div>
-      </TitleContext.Provider>
-    );
-  }
-}
+  return (
+    <TitleContext.Provider value={{ title: appTitle }}>
+      <div className="App">
+        <header className="App-header">
+          <Title>{appTitle}</Title>
+          <div>
+            <input value={appTitle} onChange={handleChange} />
+          </div>
+          <img data-type="logo" src={logo} className="App-logo" alt="logo" />
+          {open && (
+            <Counter
+              title={appTitle}
+              arrProp={['Good', 'Neutral', 'Bad']}
+              onTitleUpdate={() => setAppTitle('New Value')}
+            />
+          )}
+          <div>
+            <button onClick={handleClick}>{open ? 'Close' : 'Open'}</button>
+          </div>
+          <Table />
+        </header>
+      </div>
+    </TitleContext.Provider>
+  );
+};
